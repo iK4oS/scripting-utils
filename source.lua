a9acd19822de01251755306d8a1dbf98ba4c2f26
K4 <47014074+iK4oS@users.noreply.github.com>
2022-03-07 21:17:52 +0000

    Create source.lua

---
+++ source.lua
@@ -0,0 +1,5 @@
+local function httpRequest(url)
+	if syn and syn.request then return syn.request({Url=url})
+	elseif request then return request({Url=url})
+	else return game:HttpGetAsync(url) end
+end
