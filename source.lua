ad6322a5047c0dfd116f55bc5ed5d6831d5afae6
K4 <47014074+iK4oS@users.noreply.github.com>
2022-03-07 22:26:05 +0100

    Update source.lua

--- source.lua
+++ source.lua
@@ -1,4 +1,4 @@
-local function httpRequest(url)
+function httpRequest(url)
 	if syn and syn.request then return syn.request({Url=url})
 	elseif request then return request({Url=url})
 	else return game:HttpGetAsync(url) end
